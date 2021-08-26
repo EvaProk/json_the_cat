36aef044d5cbb347696544b6d7eb34d4f6a0daa5
Eva Prokhorenko <evaprokhorenko@gmail.com>
2021-08-26 22:42:18 +0000

    Refactor with callback functions and add tests

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,19 +1,29 @@
 const request = require('request');
 
-const arg = process.argv.slice(2);
+// const breedName = process.argv[2];
 const host = 'https://api.thecatapi.com/';
 const path = 'v1/breeds/search?q=';
 
-request(host + path + arg[0], (error, response, body) => { 
-  if (error) {
-    console.log('Error:',error);
-    return;
-  }
-  const data = JSON.parse(body);
+const fetchBreedDescription = function(breedName, callback) {
 
-  if (response.statusCode === 404 || data.length === 0) {
-    console.log('Error: Breed not found');
-    return;
-  }
-  console.log(data[0]['description']);
-});
+  request(host + path + breedName, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    const data = JSON.parse(body);
+ 
+
+    if (response.statusCode === 404 || data.length === 0) {
+      // console.log();
+      callback('Error: Breed not found', null);
+      return;
+    }
+  
+    callback(null, data[0]['description']);
+  });
+
+};
+
+
+module.exports = { fetchBreedDescription };
